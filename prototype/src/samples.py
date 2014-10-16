7018ac35539a7ab3c6ab3bba6e52ae71896b25e8
Michał 'Khorne' Rzechonek <michal@rzechonek.net>
2014-10-16 19:45:17 +0200

    Experiments with unboud callbacks.
    I've changed the semantics of callback functions to make signal handlers
    receive the same arguments as the "operation" function, plus additional
    ones like "result", "progress" etc.
    
    This allows binding methods as callbacks, but impacts callback signature
    used with regular functions.
    
    Not sure if this is good or bad, it might be that callbacks for free
    functions should know what the arguments were... up for discussion I
    guess.

--- prototype/src/samples.py
+++ prototype/src/samples.py
@@ -1,19 +1,21 @@
 import time
 from tasks import uitask, Indefinite, Progressive, progress
 
-
-def callback(result):
+def long_operation_callback(result):
     print('callback result: {}'.format(result))
 
+def long_operation_with_arguments_callback(p1, result):
+    print('callback {} result: {}'.format(p1, result))
 
-@uitask(Indefinite, finished=callback)
+
+@uitask(Indefinite, finished=long_operation_callback)
 def long_operation():
     time.sleep(3)
     return 'a'
 long_operation.description = 'running without reporting progress'
 
 
-@uitask(Progressive, finished=callback)
+@uitask(Progressive, finished=long_operation_callback)
 def long_operation_with_progress():
     count = 0
     while count < 10:
@@ -24,7 +26,7 @@ def long_operation_with_progress():
 long_operation_with_progress.description = 'running and reporting progress'
 
 
-@uitask(Progressive, finished=callback)
+@uitask(Progressive, finished=long_operation_with_arguments_callback)
 def long_operation_with_arguments(p1):
     count = 0
     while count < 10:
@@ -39,10 +41,10 @@ class Downloader:
     def __init__(self, url):
         self.url = url
 
-    # def callback(self, result):
-    #     print('downloader callback result: {}'.format(result))
+    def callback(self, result):
+        print('downloader callback result: {}'.format(result))
 
-    @uitask(Progressive)
+    @uitask(Progressive, finished=callback)
     def run(self):
         for i in range(0,10):
             time.sleep(0.3)
@@ -51,7 +53,7 @@ class Downloader:
         return self.url
     run.description = 'downloading with run' # property(str, lambda self: 'downloading {}'.format(self.url))
 
-    @uitask(Progressive)
+    @uitask(Progressive, finished=callback)
     def run2(self, p1):
         for i in range(0,10):
             time.sleep(0.3)
@@ -60,7 +62,7 @@ class Downloader:
         return self.url
     run2.description = 'downloading with run2' # property(str, lambda self: 'downloading {}'.format(self.url))
 
-    @uitask(Indefinite)
+    @uitask(Indefinite, finished=callback)
     def run3(self):
         time.sleep(1)
         return self.url
@@ -70,9 +72,8 @@ class Downloader:
 def run_background_task_samples():
     # long_operation()
     long_operation_with_progress()
-    # long_operation_with_arguments('bar')
+    long_operation_with_arguments('bar')
 
-    # downloader = Downloader('http://kernel.org')
-    # downloader.run()
-    # downloader.run2('foo')
-    # downloader.run3()
+    downloader = Downloader('http://kernel.org')
+    downloader.run()
+    downloader.run2("foo")
--- prototype/src/tasks.py
+++ prototype/src/tasks.py
@@ -36,56 +36,43 @@ class Progressive(Indefinite):
     indefinite = False
 
 
-def task(klass, started=None, finished=None, progress=None):
+def uitask(klass, started=None, finished=None, progress=None):
     '''
         Decorate a function to run in background
     '''
     def decorate(operation):
+        import functools
+
+        @functools.wraps(operation)
         def wrapper(*args, **kwargs):
             description = getattr(wrapper, 'description', operation.__name__)
             task = klass(operation, description, args, kwargs)
 
+            def call(f):
+                def wrap(*a, **kwa):
+                    a = args + a
+                    kwa.update(kwargs)
+                    f(*a, **kwa)
+                return wrap
+
+            status = Application.instance().taskStatus
+
+            task.started.connect(status.on_started)
             if started:
-                task.started.connect(started)
+                task.started.connect(call(started))
 
+            task.finished.connect(status.on_finished)
             if finished:
-                task.finished.connect(finished)
+                task.finished.connect(call(finished))
 
+            task.progress.connect(status.on_progress)
             if progress:
-                task.progress.connect(progress)
+                task.progress.connect(call(progress))
 
             return Application.instance().getTaskExecutor().submit(task)
         return wrapper
     return decorate
 
-
-def uitask(klass, started=None, finished=None, progress=None):
-    '''
-        Decorate a function to run in background and report status to
-        the main window
-    '''
-    def on_started(*args, **kwargs):
-        status = Application.instance().taskStatus
-        status.on_started(*args, **kwargs)
-        if started:
-            started(*args, **kwargs)
-
-    def on_finished(*args, **kwargs):
-        status = Application.instance().taskStatus
-        status.on_finished(*args, **kwargs)
-        if finished:
-            finished(*args, **kwargs)
-
-    def on_progress(*args, **kwargs):
-        status = Application.instance().taskStatus
-        status.on_progress(*args, **kwargs)
-        if progress:
-            progress(*args, **kwargs)
-
-    return task(klass,
-                started=on_started,
-                finished=on_finished,
-                progress=on_progress)
 
 def progress(*args, **kwargs):
     '''
